9941e034eb071736e1cf3c5135a9c1253a7f1a0a
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-03-28 00:57:15 +0530

    DAY2_PRODUCT_OF_TWO_NUMBER

---
+++ day2_product_of_two_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num1,num2;
+cout<<"enter the first:";
+cin>>num1;
+
+cout<<"enter the second number:";
+cin>>num2;
+
+int sum = num1*num2;
+cout<<sum;
+
+
+
+
+}
