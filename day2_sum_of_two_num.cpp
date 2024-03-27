40acbf2f9e258f013c7f45e6e7a2bfd8e7f8cd0d
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-03-28 00:09:22 +0530

    DAY_2\DSA _BASIC_ADDITION

---
+++ day2_sum_of_two_num.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int num1,num2;
+cout<<"enter the first number: ";
+cin>>num1;
+
+cout<<"enter the second number: ";
+cin>>num2;
+
+int sum = num1+num2;
+cout<<"THE NUMBER IS "<<sum;
+
+
+
+
+
+}
