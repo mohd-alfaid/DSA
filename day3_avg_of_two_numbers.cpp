148d89d3a27fd65cb5518c310f7ec56c57b2c749
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-03-28 23:55:59 +0530

    day3_avg_of_two_numbers/DSA

---
+++ day3_avg_of_two_numbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int a,b;
+cout<<"Enter first number:";
+cin>>a;
+
+cout<<"Enter second number:";
+cin>>b;
+
+int sum = a+b;
+int avg = sum/2;
+
+cout<<"Average of number is:"<<avg;
+
+
+}
