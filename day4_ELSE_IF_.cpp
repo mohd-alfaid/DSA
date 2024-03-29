127c9345a2de402c442d3edd89ca5f88929404a7
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-03-29 19:14:46 +0530

    DAY4/using_if_else_conditions

---
+++ day4_ELSE_IF_.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int number;
+cout<< "please enter number";
+
+cin>> number;
+
+if(number>0)
+{
+    cout<<"positive number: ";
+}
+
+else if( number==0)
+{
+    cout<<"number is 0: ";
+}
+ else
+{
+cout<<" number is negative: ";
+}
+
+
+
+}
+
+
+ 
