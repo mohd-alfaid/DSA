64ded7245afd682559c459b1f8d16db8485eac45
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-03-30 23:46:54 +0530

    DSA/LOOP_FOR_N_NATURAL_NUMBER

---
+++ day4_for_loop_n_natural_number.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+int n;
+cout<<"enter number ";
+cin>>n;
+for(int count=1;count <=n; count=count+1)
+{
+
+cout<<count<<endl;
+
+}
+
+
+
+
+}
