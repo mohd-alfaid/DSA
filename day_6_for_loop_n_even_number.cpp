d81a3bd653767f58b90dd854a33d77ffbbcb0c29
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-03-30 23:49:53 +0530

    DSA/LOOP_FOR_N_EVEN_NUMBER

---
+++ day_6_for_loop_n_even_number.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+
+int n;
+cout<<"enter the number";
+cin>>n;
+
+for(int count =0; count<=n; count= count+2)
+
+{
+
+cout<<count<<endl;
+
+}
+
+
+
+}
