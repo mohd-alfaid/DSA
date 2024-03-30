ccd42576e9a42be02e5a3fca7a4db06d9ee9bc3e
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-03-31 00:47:50 +0530

    DSA/COMPARING_TWO_NUMBERS

---
+++ day6_if_else_comparing_two_variable.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int a, b;
+ cout<<"please enter first number ";
+ cin>>a;
+ cout<<"please enter second number";
+
+ cin>>b;
+
+ if(a>b)
+ {
+    cout<<"yes";
+ }
+else
+{
+    cout<<"no";
+}
+
+
+
+
+
+
+}
