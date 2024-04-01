7675a0bdd73ca8972d1794c21c28476344be4c83
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-04-02 00:03:26 +0530

    DSA/day7_for_loop_n_natural_square

---
+++ day7_for_loop_n_natural_square.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+int n;
+cout<<"Enter the number:";
+
+cin>>n;
+for(int count=1; count<=n; count = count+1)
+{
+
+cout<<count<<"square is: "<<count*count<<endl;
+}
+
+
+}
