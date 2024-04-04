d6b7082a9d60f960ff76f212338cd3143a77cb90
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-04-04 10:49:30 +0530

    DSA/day9sum_of_n_numbers_by_formula

---
+++ day9sum_of_n_numbers_by_formula.cpp
@@ -0,0 +1,21 @@
+ #include<iostream>
+ using namespace std;
+
+ int main()
+ {
+    int n,sum;
+
+cout<<"enter the number ";
+
+cin>>n;
+ sum =0;
+for(int i = 1;i<=n; i =i+1)
+
+{
+sum = sum+i*i;
+
+}
+cout<<sum;
+
+
+ }
