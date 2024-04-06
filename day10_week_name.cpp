dd5b6fdceae3b2bf6a5c3009f4d71bea7da71155
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-04-06 23:48:36 +0530

    DSA/day10_week_name

---
+++ day10_week_name.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  
+   int num;
+
+    {cout<<"enter the number";}
+
+cin>>num;
+
+
+if(num==1)
+
+{cout<<"mon";}
+
+else if(num==2)
+
+{cout<<"tue";}
+
+else if(num==3)
+
+{cout<<"wed";}
+
+else if(num==4)
+
+cout<<"thursday";
+else if(num==5)
+
+cout<<"friday";
+else if(num==6)
+
+cout<<"sat";
+
+else if (num==7)
+{
+ cout<<"sunday";   
+
+}
+
+ if (num>7)
+ cout<<"chand p rehta h kya lole";
+ 
+if(num<0)
+cout<<"chla ja bsdk";
+
+}
