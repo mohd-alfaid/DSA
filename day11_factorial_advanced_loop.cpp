308d5e515ca4bcebdf54a460bbefc81a946d2c1e
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-04-09 00:48:15 +0530

    DSA/day11_factorial_advanced_loop

---
+++ day11_factorial_advanced_loop.cpp
@@ -0,0 +1,29 @@
+ #include<iostream>
+
+ using namespace std;
+
+ int main ()
+
+ {
+
+int n;
+long fact=1.0;
+
+cout<<"enter the number";
+
+cin>>n;
+
+for(int i=2;i<=n;i=i+1)
+
+{
+
+ 
+    fact  = fact*i;
+}
+
+cout<<fact;
+
+return 0;
+ }
+
+ /*40 factorial to store hi nhi hora pls check */
