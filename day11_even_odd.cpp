89f0d449e287a40ec1a9707660fd4730ec052d5f
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-04-09 00:47:17 +0530

    DSA/day11_even_odd

---
+++ day11_even_odd.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int num;
+cout<<"please enter the number ";
+cin>>num;
+ 
+
+if(num % 2  == 0)
+
+{ cout<< "even number";
+
+}
+
+else 
+{cout<< "odd number";
+
+}
+
+}
