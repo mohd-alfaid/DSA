d06a2945e38d4cdfd2f51521a0f6c5d7a51c08f4
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-04-11 22:10:58 +0530

    DSA/day12_print_table

---
+++ day12_print_table.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n,i;
+cout<<"enter the number";
+
+cin>>n;
+
+for( i = 1; i<=10; i = i+1)
+
+{
+cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+
+
+}
+
+
+
+
+
+
+ 
+}
+
