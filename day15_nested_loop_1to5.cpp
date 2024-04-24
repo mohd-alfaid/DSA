9b3463b3f7118cc0be3c84fb24646626875d3ea4
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-04-24 17:12:14 +0530

    DSA/day15_nested_loop_1to5

---
+++ day15_nested_loop_1to5.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+int row,col;
+
+
+for(row=1;row<=5;row=row+1)
+ 
+{
+    
+for(col=1;col<=5;col=col+1)
+ 
+ cout<<row<<" ";/*agar verrtically ya mtlb coloumn ke
+  around chaiye to column ko cout m daal do , vrna row ko daal do */
+ cout<<endl;
+
+}
+ 
+}
