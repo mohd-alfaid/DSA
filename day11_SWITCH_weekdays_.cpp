acde54ff4d95fa75c88a25123eefcb4c54e77bb1
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-08-08 16:10:51 +0530

    Switch used for weekdays print

---
+++ day11_SWITCH_weekdays_.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i;
+cout<<" enter the number";
+cin>>i;
+switch(i)
+{
+case 1:
+cout<<"mon";
+break;
+
+case 2:
+cout<<"tue";
+break;
+
+case 3:
+cout<<"wed";
+break;
+
+case 4:
+cout<<"thurs";
+break;
+
+case 5:
+cout<<"friday";
+break;
+
+case 6:
+cout<<"sat";
+break;
+
+case 7:
+cout<<"sun";
+break;
+
+
+default :
+cout<<" enter the valid date";
+
+
+
+
+}
+                 
+
+
+
+}
