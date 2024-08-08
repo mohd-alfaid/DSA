1a9506c705259e42fcbee280a0b4b7a572932c2e
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-08-08 15:58:50 +0530

    logoical operator in cpp

---
+++ day10_logical_operator.cpp
@@ -0,0 +1,27 @@
+//&&, ||, !
+
+#include <iostream>
+using namespace std;
+int main (){
+
+//logical operator 
+
+int a,b,c;
+cout<<" enter the input for a";
+cin>>a;
+cout<< " enter the input for b";
+cin>>b;
+ cout<<" enter the input for c";
+ cin>>c;
+
+ if(a>b&&a>c)
+
+ cout<<" yes ";
+ else
+ cout<<" no ";
+ 
+
+
+
+
+}
