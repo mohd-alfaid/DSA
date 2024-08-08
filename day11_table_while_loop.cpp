847034c74bd514a6cdb1b9222d4d66845cd003ba
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-08-08 16:04:55 +0530

    Table while loop

---
+++ day11_table_while_loop.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int sum , n;
+cout<<"enter the number";
+cin>>n;
+
+int i=1;
+
+while(i<=10)
+{
+
+sum= n*i;
+ 
+ cout<<sum<<endl;
+
+ i++;
+
+
+}
+
+
+
+
+
+
+}
