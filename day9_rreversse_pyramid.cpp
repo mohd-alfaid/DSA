0339e2a484062cde4631de40f94f183e60c9c69e
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-08-08 15:44:44 +0530

    reverse pyramid pattern

---
+++ day9_rreversse_pyramid.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+int row,col;
+int n;
+cout<<" enter the input :";
+cin>>n;
+
+for(row=n;row>=1; row=row-1)
+{
+    //space print
+    for(col=1;col<=n-row; col=col+1)
+    cout<<"  ";
+    //star print
+    for(col=1;col<=2*row-1;col=col+1)
+    cout<<"* ";
+    cout<<endl;
+}
+
+
+
+
+
+
+}
