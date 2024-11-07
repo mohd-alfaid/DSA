3ccdcd904cc2c32ff55d2aa3e2f0206a994cf187
MOHD ALFAID <162976521+mohd-alfaid@users.noreply.github.com>
2024-11-08 02:58:04 +0530

    Update day20_bubble_sort.cpp

--- day20_bubble_sort.cpp
+++ day20_bubble_sort.cpp
@@ -14,8 +14,8 @@ for(int i =0; i<n; i++)
     cin>>arr[i];
     }
 
-for(int i = n-2; i>=0; i--)
-{
+     for(int i = n-2; i>=0; i--)
+  {
 
     bool swapped = 0;
     for(int j=0; j<=i; j++)
@@ -27,15 +27,13 @@ for(int i = n-2; i>=0; i--)
             swap(arr[j],arr[j+1]);
 
         }
-    }
-
-    if(swapped==0)
-    break;
+       }
+  
+      if(swapped==0)
+         break;
 }
-for(int i=0; i <n; i++)
+       for(int i=0; i <n; i++)
 
-cout<<arr[i]<<"  ";
-
-
+         cout<<arr[i]<<"  ";
 
 }
